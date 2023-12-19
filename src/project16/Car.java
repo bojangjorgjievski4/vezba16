a2327e3df545b1637af6827dc0599c147ee449d0
bojangjorgjievski4 <bojangjorgjievski399@gmail.com>
2023-12-19 13:06:53 +0100

    first commit\

---
+++ src/project16/Car.java
@@ -0,0 +1,34 @@
+package project16;
+
+public class Car {
+
+		private String make;
+		private String model;
+		private int year;
+		public Car() {
+		this.make = "Nepoznato";
+		this.model = "Nepoznato";
+		this.year = 2015;
+		}
+		public Car(String make, String model, int year) {
+		this.make = make;
+		this.model = model;
+		this.year = year;
+		}
+		public String getMake() {
+		return make;
+
+		}
+		public String getModel() {
+		return model;
+
+		}
+		public int getYear() {
+		return year;
+
+		}
+		public String toString() {
+		return "Car - kola=" + make + ", model=" + model + ", godina=" + year + "]";
+
+		}
+		}
---
+++ src/project16/CarApp.java
@@ -0,0 +1,17 @@
+package project16;
+
+public class CarApp {
+
+public static void main(String[] args) {
+
+Car defaultCar = new Car();
+
+Car mojaCar = new Car("Citroen", "C4", 2016);
+
+System.out.println("Default kola : " + defaultCar);
+
+System.out.println("Odbrana kola : " + mojaCar);
+
+}
+
+}
